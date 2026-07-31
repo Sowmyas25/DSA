8375ca832cb81a00c33320cdbe7838d6bca32a4c
Sowmyas25 <163633463+Sowmyas25@users.noreply.github.com>
2026-07-31 21:29:08 +0530

    Time: 9 ms (94.22%) | Memory: 48.3 MB (17.34%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,22 +1,35 @@
 class Solution {
     public int minimumPushes(String word) {
-        int[] freq = new int[26];
-        for (char c : word.toCharArray()) {
-            freq[c - 'a']++;
-        }
+        // int[] freq = new int[26];
+        // for (char c : word.toCharArray()) {
+        //     freq[c - 'a']++;
+        // }
 
-        // Collect non‑zero frequencies and sort descending
-        List<Integer> counts = new ArrayList<>();
-        for (int f : freq) {
-            if (f > 0) counts.add(f);
-        }
-        Collections.sort(counts, Collections.reverseOrder());
+        // // Collect non‑zero frequencies and sort descending
+        // List<Integer> counts = new ArrayList<>();
+        // for (int f : freq) {
+        //     if (f > 0) counts.add(f);
+        // }
+        // Collections.sort(counts, Collections.reverseOrder());
 
-        int pushes = 0;
-        for (int i = 0; i < counts.size(); i++) {
-            int position = i / 8 + 1;        
-            pushes += counts.get(i) * position;
+        // int pushes = 0;
+        // for (int i = 0; i < counts.size(); i++) {
+        //     int position = i / 8 + 1;        
+        //     pushes += counts.get(i) * position;
+        // }
+        // return pushes;
+
+        int ans=0;
+        int[] fre=new int[26];
+        int n=word.length();
+        for(int i=0;i<n;i++){
+            char ch=word.charAt(i);
+            fre[ch-'a']++;
         }
-        return pushes;
+        Arrays.sort(fre);
+        for(int i=25;i>=0;i--){
+            ans+=fre[i]*((25-i)/8+1);
+        }
+        return ans;
     }
 }
