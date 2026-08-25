4b04d2fccf5ceafd4ea930bb772b5e004138abda
Sowmyas25 <163633463+Sowmyas25@users.noreply.github.com>
2026-08-25 20:08:14 +0530

    Time: 2 ms (68.20%) | Memory: 45.5 MB (38.11%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -1,17 +1,30 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
-        Set<Integer> numSet = new HashSet<>();
+        // Set<Integer> numSet = new HashSet<>();
+        // for (int num : nums) {
+        //     numSet.add(num);
+        // }
+        
+        // int multiple = k;
+        
+        // // Find the first multiple not in the set
+        // while (numSet.contains(multiple)) {
+        //     multiple += k;
+        // }
+        
+        // return multiple; 
+
+        Set<Integer> set = new HashSet<>();
         for (int num : nums) {
-            numSet.add(num);
+            set.add(num);
         }
-        
-        int multiple = k;
-        
-        // Find the first multiple not in the set
-        while (numSet.contains(multiple)) {
-            multiple += k;
+
+        // Check positive multiples of k in increasing order
+        for (int i = 1; ; i++) {
+            int multiple = i * k;
+            if (!set.contains(multiple)) {
+                return multiple;
+            }
         }
-        
-        return multiple; 
     }
 }
